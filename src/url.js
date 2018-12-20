75741108727fbbfd3b73238316affe57250e2e18
Klaus Sinani <klauscfhq@protonmail.com>
2018-12-20 23:31:44 +0200

    Added `url` module

---
+++ src/url.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  app: 'https://todo.microsoft.com/?app',
+  community: 'https://gitter.im/klaussinani/ao',
+  homepage: 'https://klaussinani.github.io/ao',
+  issue: 'https://github.com/klaussinani/ao/issues/new',
+  keyboardShortcutsRef: 'https://github.com/klaussinani/ao#keyboard-shortcuts',
+  license: 'https://github.com/klaussinani/ao/blob/master/license.md',
+  release: 'https://github.com/klaussinani/ao/releases/latest',
+  search: 'https://github.com/search?q=+is:issue+repo:klaussinani/ao',
+  searchFeatureRequests: 'https://github.com/klaussinani/ao/labels/feature-request',
+  source: 'https://github.com/klaussinani/ao',
+  update: 'https://klaussinani.github.io/ao/update.json'
+};
