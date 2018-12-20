13a1831dd1827eac03944d4b91b034b2be8db727
Klaus Sinani <klauscfhq@protonmail.com>
2018-12-20 23:37:15 +0200

    Added default configuration files

---
+++ src/configs/win32.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  'shortcutKeys': {
+    'add-due-date': 'Ctrl+Shift+T',
+    'add-my-day': 'Ctrl+K',
+    'complete-todo': 'Ctrl+Shift+N',
+    'delete-list': 'Ctrl+Shift+D',
+    'delete-todo': 'Ctrl+D',
+    'global-new-todo': 'Shift+Alt+C',
+    'global-search': 'Shift+Alt+F',
+    'global-toggle-ao': 'Shift+Alt+A',
+    'hide-todo': 'Ctrl+Shift+H',
+    'my-day': 'Ctrl+M',
+    'new-list': 'Ctrl+L',
+    'new-todo': 'Ctrl+N',
+    'rename-list': 'Ctrl+Y',
+    'rename-todo': 'Ctrl+T',
+    'return': 'Esc',
+    'set-reminder': 'Ctrl+Shift+E',
+    'settings': 'Ctrl+,',
+    'sign-out': 'Ctrl+Alt+Q',
+    'toggle-black-mode': 'Ctrl+B',
+    'toggle-cortana': 'Ctrl+E',
+    'toggle-dark-mode': 'Ctrl+H',
+    'toggle-sepia-mode': 'Ctrl+G',
+    'toggle-sidebar': 'Ctrl+O'
+  }
+};
