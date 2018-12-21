beb75f192d8a9db6017d37ca406cd4ac31eab16a
Klaus Sinani <klauscfhq@protonmail.com>
2018-12-21 22:30:05 +0200

    Added `keymap` module

---
+++ src/keymap.js
@@ -0,0 +1,42 @@
+'use strict';
+const {globalShortcut} = require('electron');
+const {shortcutKeys} = require('./config');
+const win = require('./win');
+
+const {log} = console;
+
+class Keymap {
+  setAcc(custom, predefined) {
+    if (Object.prototype.hasOwnProperty.call(shortcutKeys, custom)) {
+      return shortcutKeys[custom];
+    }
+    return predefined;
+  }
+
+  registerGlobal() {
+    const toggleAo = globalShortcut.register(
+      this.setAcc('global-toggle-ao', 'Shift+Alt+A'), () => {
+        win.toggle();
+      });
+
+    const searchTodo = globalShortcut.register(
+      setAcc('global-search', 'Shift+Alt+F'), () => {
+        win.appear();
+        win.activate('search');
+      });
+
+    const createTodo = globalShortcut.register(
+      setAcc('global-new-todo', 'Shift+Alt+C'), () => {
+        win.appear();
+        win.activate('new-todo');
+      });
+
+    if (toggleAo && searchTodo && createTodo) {
+      log('Successfully registered global shortcut keys');
+    } else {
+      log('Global shortcut keys registration failed');
+    }
+  }
+}
+
+module.exports = new Keymap();
