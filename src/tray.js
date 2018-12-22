421116c84378743d9e9e71d76ce45ead464ccff5
Klaus Sinani <klauscfhq@protonmail.com>
2018-12-23 00:01:02 +0200

    Minor tweaks on `tray` module

--- src/tray.js
+++ src/tray.js
@@ -8,15 +8,19 @@ const win = require('./win');
 const {app, Menu} = electron;
 
 class Tray {
+  constructor() {
+    this._tray = null;
+  }
+
   create() {
     if (is.darwin) {
       return;
     }
 
-    const tray = new electron.Tray(file.trayIcon);
-    tray.setToolTip(app.getName());
-    tray.setContextMenu(Menu.buildFromTemplate(template));
-    tray.on('click', win.toggle);
+    this._tray = new electron.Tray(file.trayIcon);
+    this._tray.setToolTip(app.getName());
+    this._tray.setContextMenu(Menu.buildFromTemplate(template));
+    this._tray.on('click', win.toggle);
   }
 }
 
