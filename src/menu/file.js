ee94c601936c102e7da359694c8ff9528d8994d3
Klaus Sinani <klauscfhq@protonmail.com>
2019-01-20 21:03:23 +0200

    Toggle checkbox only on confirmed option update

--- src/menu/file.js
+++ src/menu/file.js
@@ -173,8 +173,8 @@ module.exports = {
       type: 'checkbox',
       checked: settings.get('useGlobalShortcuts'),
       click(item) {
-        settings.set('useGlobalShortcuts', item.checked);
-        dialog.confirmRestart();
+        dialog.confirmActivationRestart('useGlobalShortcuts', item.checked);
+        item.checked = settings.get('useGlobalShortcuts');
       }
     }, {
       label: 'Request Exit Confirmation',
